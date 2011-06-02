55b34cf981c2f5b7f5044f79917ae55317fc55f6
luke <luke@inertiabev.com>
2011-06-01 22:53:11 -0700

    more stuff

--- graph-util.lisp
+++ graph-util.lisp
@@ -7,6 +7,11 @@
                         (attic (you are in the attic.
                                     there is a giant welding torch in the corner.))))
 
+(defparameter *edges* '((living-room (garden west door)
+                                (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+
 (defparameter *drink-order* '((bill . double-espresso)
                               (lisa . small-drip-coffee)
                               (john . medium-latte)))
